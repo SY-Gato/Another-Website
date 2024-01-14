3904bae0f1bda000bbc733bc56b154f7c55fa99e
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-01-13 22:48:01 -0600

    Update mainSite.js

--- mainSite.js
+++ mainSite.js
@@ -13,3 +13,17 @@ switchButton.addEventListener("click", function (event) {
   if (!event.target.id) return;
   update(event.target.id);
 });
+
+const update = (tabId) => {
+  const currentTab = document.querySelector(".active");
+  if (currentTab.id != tabId) {
+    currentTab.classList.remove("active");
+  }
+  const selectTab = document.getElementById(tabId);
+  selectTab.classList.add("active");
+  const entry = data.get(tabId);
+  if (entry) {
+    history.pushState(null, "", entry.url);
+    contenr.innerHtml = entry.content;
+  }
+};
