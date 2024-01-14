bef220f485e232b97619ef616deb1e3408e1dc2d
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-01-13 22:41:26 -0600

    Update mainSite.js

--- mainSite.js
+++ mainSite.js
@@ -1,1 +1,15 @@
 const switchButton = document.getElementById("SwitchSite");
+
+const data = new Map([
+  [
+    "SwitchSite",
+    {
+      url: "index.html#site2",
+      content: "Second Page!",
+    },
+  ],
+]);
+switchButton.addEventListener("click", function (event) {
+  if (!event.target.id) return;
+  update(event.target.id);
+});
