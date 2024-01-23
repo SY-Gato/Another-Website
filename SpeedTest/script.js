7a148d3d28174211323c10fa2b042b20659c5ade
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-01-22 23:19:12 -0600

    Update script.js

--- SpeedTest/script.js
+++ SpeedTest/script.js
@@ -9,7 +9,7 @@ let info = document.getElementById("info");
 let totalBitSpeed = 0;
 let totalKbSpeed = 0;
 let totalMbSpeed = 0;
-let numTests = 1;
+let numTests = 10;
 let testCompleted = 0;
 
 // Get random image from unsplash.com
